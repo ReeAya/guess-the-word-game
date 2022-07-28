63cca55be4fd008ae3b1643fd4ffd2a14ed431c0
reezan_s <reezansmith@gmail.com>
2022-07-29 08:45:36 +0900

    Added JS to display word and guessed letters

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
-const guessedLettersElement = document.querySelector(".guessed-letters");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
@@ -10,6 +10,7 @@ const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
 const guessedLetters = [];
 
+// Display our symbols as placeholders for the chosen word's letters
 const placeholder = function(word) {
     const placeholderLetters = [];
     for (const letter of word) {
@@ -23,20 +24,33 @@ placeholder(word);
 
 guessLetterButton.addEventListener("click", function(e) {
     e.preventDefault();
+    // Empty message paragraph
+    message.innerText = "";
+    // Let's grab what was entered in the input
     const guess = letterInput.value;
-    console.log(guess);
+    // Let's make sure that it is a single letter
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+        // We've got a letter! Let's guess!
+        makeGuess(guess);
+    }
     letterInput.value = "";
 });
 
 const validateInput = function(input) {
-    const acceptLetter = /[a-zA-Z]/;
+    const acceptedLetter = /[a-zA-Z]/;
     if (input.length === 0) {
+        // Is the input empty?
         message.innerText = "Please enter a letter.";
     } else if (input.length > 1) {
-        message.inner = "Please enter a single letter.";
+        // Did you type more than one letter?
+        message.innerText = "Please enter a single letter.";
     } else if (!input.match(acceptedLetter)) {
+        // Did you type a number, a special character or some other non letter thing?
         message.innerText = "Please enter a letter from A to Z.";
     } else {
+        // We finally got a single letter, omg yay
         return input;
     }
 };
@@ -44,9 +58,44 @@ const validateInput = function(input) {
 const makeGuess = function(guess) {
     guess = guess.toUpperCase();
     if (guessedLetters.includes(guess)) {
-        message.innerText = "You already guessed that letter. Try again.";
+        message.innerText = "You already guessed that letter, silly. Try again.";
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
+    }
+};
+
+const showGuessedLetters = function() {
+    // Clear the list first
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+const updateWordInProgress = function(guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    // console.log(revealWord);
+    wordInProgress.innerText = revealWord.join("");
+    checkIfWin();
+};
+
+const checkIfWin = function() {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
     }
 };
